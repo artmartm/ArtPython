28cbbce655af02fff680abd49b4d8cbe1e8e7b5c
ArtPython <python-python@list.ru>
2021-10-04 17:41:11 +0300

    split tasks and made changes

---
+++ iterator_task_#1.py
@@ -0,0 +1,42 @@
+from icecream import ic
+
+
+# first way
+class Counter:
+
+    def __init__(self, start, stop):
+        if isinstance(start and stop, int):
+            self.start = start - 1
+            self.stop = stop - 1
+        else:
+            raise ValueError('start and stop have to be integers!')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start < self.stop:
+            self.start += 1
+            return self.start
+        raise StopIteration
+
+
+# second way
+def my_counter(start, stop):
+    if isinstance(start and stop, int):
+
+        start -= 1
+        while stop - 1 > start:
+            start += 1
+            yield start
+    else:
+        raise ValueError('start and stop have to be integers!')
+
+
+if __name__ == "__main__":
+    ic('first way')
+    for element in Counter(1, 5):
+        ic(element)
+    ic('second way')
+    for element in my_counter(1, 5):
+        ic(element)
---
+++ iterator_task_#2.py
@@ -0,0 +1,21 @@
+from icecream import ic
+
+
+def while_as_for(lst):
+    try:
+        iter_for_books = iter(lst)
+
+        while True:
+            try:
+                book = next(iter_for_books)
+                ic(book)
+            except StopIteration:
+                break
+    except TypeError:
+        raise TypeError('object has to be iterable!')
+
+
+if __name__ == "__main__":
+    books = ['Atlas Shrugged', 'Tango with django ', 'Python crash course']
+
+    while_as_for(books)
