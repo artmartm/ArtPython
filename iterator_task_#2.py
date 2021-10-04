23601a777ac127f4ec154fc16e8758d5e920a211
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-10-04 19:48:10 +0300

    fixed task #2

--- iterator_task_#2.py
+++ iterator_task_#2.py
@@ -1,6 +1,3 @@
-from icecream import ic
-
-
 def while_as_for(lst):
     try:
         iter_for_books = iter(lst)
@@ -12,9 +9,3 @@ def while_as_for(lst):
                 break
     except TypeError:
         raise TypeError('object has to be iterable!')
-
-
-if __name__ == "__main__":
-    books = ['Atlas Shrugged', 'Tango with django ', 'Python crash course']
-
-    while_as_for(books)
