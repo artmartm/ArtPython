375bc1f08fb3be02f2c1faaa05708e0267d30e88
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-03 14:07:54 +0300

    Update models.py
    make the name field specific, add blank=True to the text field, rename general models

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator
 from datetime import date
 
 
-class SoftDeletedModel(models.Model):
+class SoftDeletable(models.Model):
     """Abstract soft deleted model"""
     deleted = models.BooleanField(default=False)
 
@@ -13,7 +13,7 @@ class SoftDeletedModel(models.Model):
         abstract = True
 
 
-class CreatedAtModel(models.Model):
+class Creatable(models.Model):
     """Abstract created at model"""
     created_at = models.DateTimeField(default=now, validators=[MaxValueValidator(limit_value=now)])
 
@@ -21,38 +21,45 @@ class CreatedAtModel(models.Model):
         abstract = True
 
 
-class GeneralFieldsModel(models.Model):
+class GeneralFields(models.Model):
     """Abstract model with general fields"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=30)
     objects = models.Manager()
-
-    def __str__(self):
-        return self.name
 
     class Meta:
         abstract = True
 
 
-class Genre(CreatedAtModel, SoftDeletedModel, GeneralFieldsModel):
+class Genre(Creatable, SoftDeletable, GeneralFields):
     """Child model for genres"""
-    pass
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 
-class Author(CreatedAtModel, SoftDeletedModel, GeneralFieldsModel):
+class Author(Creatable, SoftDeletable, GeneralFields):
     """Child model for authors"""
+    name = models.CharField(max_length=30)
     year_of_birth = models.DateField(validators=[MaxValueValidator(limit_value=date.today)])
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
-class Book(CreatedAtModel, SoftDeletedModel, GeneralFieldsModel):
+
+class Book(Creatable, SoftDeletable, GeneralFields):
     """Child model for books"""
+    name = models.CharField(max_length=30)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     page_count = models.IntegerField(default=0)
     year = models.DateField(default=now, validators=[MaxValueValidator(limit_value=date.today)])
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     prime_language = models.CharField(max_length=30)
-    text = models.TextField(default='There is no text yet')
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 from .queries import CustomBookQuerySet, CustomAuthorQuerySet, CustomGenreQuerySet  # I know that it isn't a good thing
