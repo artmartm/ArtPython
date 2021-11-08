e09382eed92c3803d7b8ba4f59e1f62e19a46b75
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-08 10:28:41 +0300

    Create serializers.py

---
+++ serializers.py
@@ -0,0 +1,52 @@
+from .models import League, Player, Team
+from rest_framework import serializers
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+
+class LeagueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = '__all__'
+
+
+class PlayerDetailSerializer(serializers.ModelSerializer):
+    current_team = TeamSerializer()
+
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+
+class TeamDetailSerializer(TeamSerializer):
+    players = serializers.SerializerMethodField()
+    league = LeagueSerializer()
+
+    @staticmethod
+    def get_players(team):
+        """return nested list of players for the team"""
+        players = PlayerSerializer(Player.objects.filter(current_team=team), many=True).data
+        list_of_players = [player['name'] for player in players]
+        return list_of_players
+
+
+class LeagueDetailSerializer(LeagueSerializer):
+    teams = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_teams(league):
+        """return nested list of teams for the league"""
+
+        teams = TeamDetailSerializer(Team.objects.filter(league=league), many=True).data
+        list_of_teams = [team['name'] for team in teams]
+        return list_of_teams
