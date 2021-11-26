347bc2ee39d7c853047f51c67113e074f5344178
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-26 16:45:38 +0300

    Create serializers.py

---
+++ Project/backend/apps/leagues/serializers.py
@@ -0,0 +1,21 @@
+from .models.models import League
+from rest_framework import serializers
+# from settings.apps.teams.serializers import TeamSerializers
+from apps.teams.models.models import Team
+from apps.teams.serializers import TeamSerializers
+
+
+class LeagueSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = League
+        fields = '__all__'
+
+
+class LeagueDetailSerializer(LeagueSerializers):
+    teams = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_teams(league):
+        """return nested list of teams for the league"""
+        teams = TeamSerializers(Team.objects.filter(league=league), many=True).data
+        return [team['name'] for team in teams]
