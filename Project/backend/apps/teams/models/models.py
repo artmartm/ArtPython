7a4ff8d5d9f612c845b6ab1c0218cde0e1496352
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-29 17:04:17 +0300

    Update models.py

--- Project/backend/apps/teams/models/models.py
+++ Project/backend/apps/teams/models/models.py
@@ -6,7 +6,7 @@ from apps.general.models.choices import SPORT_BRANDS
 
 class Team(StillActive, BaseModel, PLTSBaseModel):
     name = models.CharField(max_length=30)
-    team_logo = models.ImageField(blank=True, null=True, upload_to='teams/')
+    team_logo = models.ImageField(blank=True, null=True)
     history = models.TextField()
     league = models.ForeignKey(League, on_delete=models.CASCADE)
     description = models.TextField()
@@ -16,16 +16,32 @@ class Team(StillActive, BaseModel, PLTSBaseModel):
     sponsors = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
     sport_brand = models.CharField(max_length=30, choices=SPORT_BRANDS)
+    score = models.PositiveIntegerField(default=10)
 
     #######
     @property
     def games(self):
         return len(Game.objects.filter(t1=self) | Game.objects.filter(t2=self))
 
-    #####
+    @property
+    def won(self):
+        #return len(queryset.annotate(members=Count('players')).filter(members=value))
+        return Game.objects.values_list('wins', flat=True).get(t1=self.id)|Game.objects.values_list('wins', flat=True).get(t2=self.id)
+    ####
 
     def __str__(self):
         return self.name
+
+
+class TeamStats(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    goals_scored = models.PositiveIntegerField()
+    goals_missed = models.PositiveIntegerField()
+    matches = models.PositiveIntegerField()
+    points = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.team}' stats" if self.team.name[-1] == 's' else f"{self.team}'s stats"
 
 
 class Stadium(StillActive, BaseModel, PLTSBaseModel):
@@ -35,7 +51,7 @@ class Stadium(StillActive, BaseModel, PLTSBaseModel):
     avg_attendence = models.PositiveIntegerField()
     description = models.TextField()
     history = models.TextField()
-    image = models.ImageField(blank=True, null=True, upload_to='teams/')
+    image = models.ImageField(blank=True, null=True)
     rebuild = models.BooleanField(default=True)
 
     def __str__(self):
@@ -47,3 +63,16 @@ class Stadium(StillActive, BaseModel, PLTSBaseModel):
 class Game(models.Model):
     t1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='t1')
     t2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='t2')
+    wins = models.CharField(max_length=50, default=t1)
+
+    @property
+    def name(self):
+        return f'play {self.t1} and {self.t2}'
+
+    @property
+    def win(self):
+        if self.t1.score>self.t2.score:
+            self.wins = self.t1
+        else:
+            self.wins = self.t2
+        return True
