b29a3bececca6b6c653c65d63f0da14c73308ca2
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-29 18:04:46 +0300

    Update views.py

--- Project/backend/apps/leagues/views.py
+++ Project/backend/apps/leagues/views.py
@@ -1,1 +1,18 @@
+from rest_framework import viewsets
+from .models.models import League
+from .serializers import LeagueSerializers, LeagueDetailSerializer
 
+
+class LeagueViewSet(viewsets.ModelViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializers
+
+    action_to_serializer = {
+        "retrieve": LeagueDetailSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_to_serializer.get(
+            self.action,
+            self.serializer_class
+        )
