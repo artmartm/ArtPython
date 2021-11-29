eecf53b1ff29d511c4a9b1fb432e7fd1bd02172e
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-29 08:44:11 +0300

    Update models.py

--- Project/backend/apps/users/models/models.py
+++ Project/backend/apps/users/models/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class User(AbstractUser):
-    tel = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.username
