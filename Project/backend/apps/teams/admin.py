f6c44034c42ae502b56bbdbf304e9a5b36c0d89d
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-29 17:05:27 +0300

    Update admin.py

--- Project/backend/apps/teams/admin.py
+++ Project/backend/apps/teams/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models.models import Team, Stadium
+from .models.models import Team, Stadium, TeamStats
 
 # Register your models here.
 admin.site.register(Team)
 admin.site.register(Stadium)
+admin.site.register(TeamStats)
