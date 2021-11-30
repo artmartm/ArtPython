18192c003b770b10b3a94fb27f2ac5d584cb24da
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-30 14:34:19 +0300

    Update models.py

--- Project/backend/apps/teams/models/models.py
+++ Project/backend/apps/teams/models/models.py
@@ -17,40 +17,52 @@ class Team(StillActive, BaseModel, PLTSBaseModel):
     second_name = models.CharField(max_length=30)
     sport_brand = models.CharField(max_length=30, choices=SPORT_BRANDS)
     score = models.PositiveIntegerField(default=10)
+    win = models.PositiveIntegerField(default=0)
 
     @property
     def games(self):
-        return len(Game.objects.filter(t1=self) | Game.objects.filter(t2=self))
+        return len(Game.objects.filter(home_team=self) | Game.objects.filter(away_team=self))
 
     @property
     def wins(self):
-        return len(Game.objects.filter(wins=self))
+        return len(Game.objects.filter(winner=self))
+
+    @property
+    def defeats(self):
+        return len(Game.objects.filter(loser=self))
 
     @property
     def points(self):
         return int(self.wins) * 2
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_team')
     away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_team')
-    wins = models.CharField(max_length=50)
-    home_team_goals = models.PositiveIntegerField()
-    away_team_goals = models.PositiveIntegerField()
+    winner = models.CharField(max_length=50, null=True, blank=True)
+    loser = models.CharField(max_length=50, null=True, blank=True)
+    home_team_goals = models.PositiveIntegerField(null=True, blank=True)
+    away_team_goals = models.PositiveIntegerField(null=True, blank=True)
 
     @property
     def name(self):
-        return f'{self.t1} vs {self.t2}'
+        return f'{self.home_team} vs {self.away_team}'
 
+    @property
     def win(self):
-        if self.t1.score > self.t2.score:
-            self.wins = self.t1.name
-            self.t1_goals = self.t1.score - self.t2.score
+        if self.home_team.score > self.away_team.score:
+            self.winner = self.home_team.name
+            self.loser = self.away_team.name
             self.save()
+            return self.home_team.name
         else:
-            self.wins = self.t2.name
-            self.t2_goals = 0
+            self.winner = self.away_team.name
+            self.loser = self.home_team.name
             self.save()
+            return self.away_team.name
 
     def __str__(self):
         return self.name
