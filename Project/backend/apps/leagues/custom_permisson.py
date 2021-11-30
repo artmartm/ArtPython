7757ca844d36a2f358bcc209761d5bbc2c16f57b
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-30 17:24:29 +0300

    Create custom_permisson.py

---
+++ Project/backend/apps/leagues/custom_permisson.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+from apps.users.models.models import userProfile
+
+GENERAL_METHODS = ('GET', 'HEAD', 'OPTIONS')
+MODERATOR_METHODS = ('GET', 'HEAD', 'OPTIONS')
+AUTHOR_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+
+class OnlyLookOrAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in GENERAL_METHODS or
+            request.user and
+            request.user.is_superuser or
+            request.user and
+            userProfile.objects.values_list('is_moderator', flat=True).get(user=request.user.id)
+        )
