04991376deac4346e1fc147f5a21838023a1d6a3
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-11-30 17:18:21 +0300

    Update models.py

--- Project/backend/apps/users/models/models.py
+++ Project/backend/apps/users/models/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import User
+from apps.general.models.generals import City, Country
+from apps.teams.models.generals import Team
+
+
+class userProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    description = models.TextField(blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    favorite_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    is_moderator = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
