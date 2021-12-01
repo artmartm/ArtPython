881918120eaf7b44bbe6e3387066634f9a84b588
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-01 10:52:36 +0300

    Update serializers.py

--- Project/backend/apps/players/serializers.py
+++ Project/backend/apps/players/serializers.py
@@ -2,6 +2,7 @@ from .models.models import Player, PlayerPersonalInfo, PlayerMainInfo
 from rest_framework import serializers
 from apps.general.models.generals import News, Comments
 from apps.general.serializers import NewsSerializers, CommentsSerializers
+from django.contrib.contenttypes.models import ContentType
 
 
 class PlayerSerializers(serializers.ModelSerializer):
@@ -31,21 +32,26 @@ class PlayerDetailSerializers(PlayerSerializers):
     news = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
     free_agent = serializers.ReadOnlyField()
+    id_player = ContentType.objects.values_list('id', flat=True).get(model='league')
 
     @staticmethod
-    def get_news(id):
-        news = NewsSerializers(News.objects.filter(content_type=19).filter(object_id=id.id), many=True).data
+    def get_news(player, id_player=id_player):
+        news = NewsSerializers(News.objects.filter(content_type=id_player).
+                               filter(object_id=player.id), many=True).data
         return news
 
     @staticmethod
-    def get_comments(id):
-        comments = CommentsSerializers(Comments.objects.filter(content_type=19).filter(object_id=id.id), many=True).data
+    def get_comments(player, id_player=id_player):
+        comments = CommentsSerializers(Comments.objects.filter(content_type=id_player).
+                                       filter(object_id=player.id), many=True).data
         return comments
 
     @staticmethod
     def get_main_info(player):
-        return PlayerMainInfoSerializers(PlayerMainInfo.objects.filter(player=player), many=True).data
+        return PlayerMainInfoSerializers(PlayerMainInfo.objects.
+                                         filter(player=player), many=True).data
 
     @staticmethod
     def get_personal_info(player):
-        return PlayerPersonalInfoSerializers(PlayerPersonalInfo.objects.filter(player=player), many=True).data
+        return PlayerPersonalInfoSerializers(PlayerPersonalInfo.objects.
+                                             filter(player=player), many=True).data
