89027943af59f89b7d091552be46b4f9cd281166
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-01 10:51:53 +0300

    Update serializers.py

--- Project/backend/apps/leagues/serializers.py
+++ Project/backend/apps/leagues/serializers.py
@@ -5,6 +5,7 @@ from apps.teams.models.models import Team
 from apps.general.models.generals import News, Comments
 from apps.teams.serializers import TeamSerializers
 from apps.general.serializers import NewsSerializers, CommentsSerializers
+from django.contrib.contenttypes.models import ContentType
 
 
 class LeagueSerializers(serializers.ModelSerializer):
@@ -17,6 +18,7 @@ class LeagueDetailSerializer(LeagueSerializers):
     teams = serializers.SerializerMethodField()
     news = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
+    id_league = ContentType.objects.values_list('id', flat=True).get(model='league')
 
     @staticmethod
     def get_teams(league):
@@ -25,11 +27,13 @@ class LeagueDetailSerializer(LeagueSerializers):
         return teams
 
     @staticmethod
-    def get_news(id):
-        news = NewsSerializers(News.objects.filter(content_type=13).filter(object_id=id.id), many=True).data
+    def get_news(league, id_league=id_league):
+        news = NewsSerializers(News.objects.filter(content_type=id_league).
+                               filter(object_id=league.id), many=True).data
         return news
 
     @staticmethod
-    def get_comments(id):
-        comments = CommentsSerializers(Comments.objects.filter(content_type=13).filter(object_id=id.id), many=True).data
+    def get_comments(league, id_league=id_league):
+        comments = CommentsSerializers(Comments.objects.filter(content_type=id_league).
+                                       filter(object_id=league.id), many=True).data
         return comments
