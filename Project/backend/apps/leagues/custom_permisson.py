563220aaf5e981e9eb6a6edb9cb46402fef8b9d3
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-01 10:51:38 +0300

    Update custom_permisson.py

--- Project/backend/apps/leagues/custom_permisson.py
+++ Project/backend/apps/leagues/custom_permisson.py
@@ -1,9 +1,8 @@
 from rest_framework import permissions
-from apps.users.models.models import userProfile
+# from apps.users.models import userProfile
+from apps.users.models import userProfile
 
 GENERAL_METHODS = ('GET', 'HEAD', 'OPTIONS')
-MODERATOR_METHODS = ('GET', 'HEAD', 'OPTIONS')
-AUTHOR_METHODS = ('GET', 'HEAD', 'OPTIONS')
 
 
 class OnlyLookOrAdmin(permissions.BasePermission):
@@ -11,8 +10,6 @@ class OnlyLookOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in GENERAL_METHODS or
-            request.user and
-            request.user.is_superuser #or
-            # request.user and
-            # userProfile.objects.values_list('is_moderator', flat=True).get(user=request.user.id)
+            request.user and userProfile.objects.values_list('is_moderator', flat=True).get(user=request.user.id) or
+            request.user and request.user.is_superuser
         )
