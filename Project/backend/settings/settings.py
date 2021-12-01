7f589f2096221cac74cdc64a71defb02e137d549
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-01 14:03:19 +0300

    uncomment debug and secret key

--- Project/backend/settings/settings.py
+++ Project/backend/settings/settings.py
@@ -24,9 +24,8 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
 ALLOWED_HOSTS = []
 
 # Application definition
