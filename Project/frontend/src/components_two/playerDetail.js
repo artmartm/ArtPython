d6357e72a7b3982e7846bdecdfb465f78b7b4def
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-02 15:15:34 +0300

    Update playerDetail.js

--- Project/frontend/src/components_two/playerDetail.js
+++ Project/frontend/src/components_two/playerDetail.js
@@ -10,7 +10,7 @@ function PlayerDetail({ match }) {
     useEffect(()=>{
         axios({
             method:'GET',
-            url:`http://127.0.0.1:8000/api/player/${id}`,
+            url:`http://127.0.0.1:8000/api/players/${id}`,
         }).then(response=>{
             setPlayer(response.data)
         })
@@ -21,11 +21,11 @@ function PlayerDetail({ match }) {
         <div>
             <h1>Player page</h1>
             <h2>{player.name}</h2>
-            <Link style={{textDecoration: 'none'}} key={player.team} to={`/team/${player.team}`} ><h2>team</h2></Link>
+            <Link style={{textDecoration: 'none'}} key={player.team} to={`/teams/${player.team}`} ><h2>team</h2></Link>
             <h2>{player.team}</h2>
             <hr/>
             {/*{player.map(e=>(
-                <Link key={e.id} to={`/player/${e.id}`} ><h2>{e.name}!!!</h2></Link>
+                <Link key={e.id} to={`/players/${e.id}`} ><h2>{e.name}!!!</h2></Link>
             ))} */}
         </div>
     )
