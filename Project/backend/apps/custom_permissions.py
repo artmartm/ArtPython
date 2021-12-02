2eae205c2abfe0b27285d0005c3a2010b176d1c7
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-02 16:50:48 +0300

    Create custom_permissions.py

---
+++ Project/backend/apps/custom_permissions.py
@@ -0,0 +1,25 @@
+from rest_framework import permissions
+#from rest_framework.permissions import IsS
+# from apps.users.models import userProfile
+from apps.users.models import userProfile
+
+GENERAL_METHODS = ('GET', 'HEAD', 'OPTIONS')
+
+
+class OnlyLookOrAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in GENERAL_METHODS or
+            request.user and request.user.is_superuser
+        )
+
+class OnlyLookOrAdminModerator(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method in GENERAL_METHODS or
+            request.user and userProfile.objects.values_list('is_moderator', flat=True).
+            get(user=request.user.id) or
+            request.user and request.user.is_superuser
+        )
