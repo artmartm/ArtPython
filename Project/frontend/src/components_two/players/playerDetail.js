765dd42ab96d84f33988fa90083fc8ade22b6381
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-08 10:43:09 +0300

    update playerDetail page

--- Project/frontend/src/components_two/players/playerDetail.js
+++ Project/frontend/src/components_two/players/playerDetail.js
@@ -3,18 +3,23 @@ import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import AddComment from "../general/comments/addComment";
 import CommentsList from "../general/comments/commentsList";
-import content_type from "../general/content_type";
 import PlayerMainInfo from "./playerMainInfo";
 import PlayerPersonalInfo from "./playerPersonalInfo";
 
 function PlayerDetail({ match }) {
-    const[state, setState] = useState([{
+    const[showComments, setShowComments] = useState([{
         isOpen:false
     }])
+    const[showPersonalInfo, setShowPersonalInfo] = useState([{
+        isOpen:false
+    }])    
+    const[showMainInfo, setShowMainInfo] = useState([{
+        isOpen:false
+    }])
+
     const[player, setPlayer] = useState({});
-    const[team,setTeam] = useState([]);
     const id = match.params.id;
-    const ct = '18';
+    const cotent_type = '18';
 
     useEffect(()=>{
         axios({
@@ -22,32 +27,50 @@ function PlayerDetail({ match }) {
             url:`http://127.0.0.1:8000/api/players/${id}`,
         }).then(response=>{
             setPlayer(response.data)
-            setTeam(response.data.team)
         })
     },[id])
 
-
     return(
         <div>
-            <h1>Player page</h1>
-            <h2>{player.name}</h2>
-            <Link style={{textDecoration: 'none'}} key={player.team} to={`/teams/${player.team}`} ><h2>team</h2></Link>
+            <h1>{player.name} page</h1>
+            <h3>team is {player.team}</h3>
+            <h3>position is {player.position}</h3>
+            <Link 
+                style={{textDecoration: 'none'}} 
+                key={player.team} 
+                to={`/teams/${player.team}`}>
+                <p>to the player's team</p>
+            </Link>
             <hr/>
-            <h2>list of comments</h2>
             <React.Fragment>
-                <button onClick={()=>{setState({isOpen:true})}}>show just</button>        
+                <button onClick={()=>{setShowComments({isOpen:true})}}>show comments</button>        
                     {state.isOpen && 
                         <div>       
                             <CommentsList key={id}  obj={id}/>
-                            <button onClick={()=>{setState({isOpen:false})}}>close </button>
+                            <button onClick={()=>{setShowComments({isOpen:false})}}>close</button>
                         </div>
                     }
             </React.Fragment>
-            <h2>leave a comment</h2>
                 <AddComment obj={id}/>
             <br/>
-            <PlayerPersonalInfo obj={player.id} />
-            <PlayerMainInfo obj={player.id} />
+            <React.Fragment>
+                <button onClick={()=>{setShowPersonalInfo({isOpen:true})}}>show personal info</button>        
+                    {state.isOpen && 
+                        <div>       
+                            <PlayerPersonalInfo obj={player.id} />
+                            <button onClick={()=>{setShowPersonalInfo({isOpen:false})}}>close</button>
+                        </div>
+                    }
+            </React.Fragment>
+            <React.Fragment>
+                <button onClick={()=>{setShowMainInfo({isOpen:true})}}>show personal info</button>        
+                    {state.isOpen && 
+                        <div>       
+                            <PlayerMainInfo obj={player.id} />
+                            <button onClick={()=>{setShowMainInfo({isOpen:false})}}>close</button>
+                        </div>
+                    }
+            </React.Fragment>
         </div>
     )
 }
