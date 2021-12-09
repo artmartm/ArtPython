09ba4d9924e785ecbe300583a58e09ca3c550391
ArtPython <91617579+ArtPython@users.noreply.github.com>
2021-12-09 15:11:29 +0300

    fix custom permissions

--- Project/backend/apps/custom_permissions.py
+++ Project/backend/apps/custom_permissions.py
@@ -1,53 +1,27 @@
 from rest_framework import permissions
-# from rest_framework.permissions import IsS
-# from apps.users.models import userProfile
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from apps.users.models.models import UserProfile
 
 GENERAL_METHODS = ('GET', 'HEAD', 'OPTIONS')
-COMMENT_ACCOUNT_METHODS = ('GET', 'HEAD', 'OPTIONS', 'POST')
 
 
-class OnlyAdmin(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return bool(
-            request.user and request.user.is_superuser
-        )
-
-
-class OnlyAdminOrModerator(permissions.BasePermission):
+class OnlyAdminOrModerator(IsAuthenticated):
 
     def has_permission(self, request, view):
-        return bool(
-            request.user.id and
-            (request.user and (request.user.is_superuser or
-                               UserProfile.objects.values_list('is_moderator', flat=True).
-                               get(user=request.user.id)))
-        )
+        return bool((request.user.is_superuser or
+                     UserProfile.objects.values_list('is_moderator', flat=True).
+                     get(user=request.user.id)))
 
 
-class OnlyLookOrAdminModerator(permissions.BasePermission):
+class OnlyLookOrAdminModerator(OnlyAdminOrModerator):
 
     def has_permission(self, request, view):
-        return bool(request.method in COMMENT_ACCOUNT_METHODS or
-                    request.user.id and (
-                            UserProfile.objects.values_list('is_moderator', flat=True).
-                            get(user=request.user.id) or
-                            request.user.is_superuser)
-                    )
+        return bool(request.user.id and request.method in GENERAL_METHODS)
 
 
-class OnlyLookOrRequestUser(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return bool(request.method in GENERAL_METHODS or
-                    request.user.id and (
-                            UserProfile.objects.values_list('is_moderator', flat=True).
-                            get(user=request.user.id) or
-                            request.user.is_superuser)
-                    )
+class OnlyLookOrRequestUser(IsAuthenticatedOrReadOnly):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in COMMENT_ACCOUNT_METHODS:
-            return True
-        return obj.author == request.user
+        permission = bool((request.user and obj.author == request.user) or
+                          (request.user and request.user.is_superuser))
+        return permission
