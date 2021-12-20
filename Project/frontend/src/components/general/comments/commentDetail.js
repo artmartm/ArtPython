5bfdac51406d0a44742ac937ed73e7cc6f83f682
ArtPython <python-python@list.ru>
2021-12-20 10:17:52 +0300

    add update form

--- Project/frontend/src/components/general/comments/commentDetail.js
+++ Project/frontend/src/components/general/comments/commentDetail.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, {useState, useEffect, useContext} from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { fetchParticularUser } from "../../../redux_two/actions/asyncActions/asyncParticularUser";
 import AuthContext from "../base/AuthContext";
 import DeleteComment from "./deleteComment";
@@ -15,9 +15,19 @@ function CommentDetail({ match }) {
     const user ='123'
     const[comment, setComment] = useState({});
     //const[team,setTeam] = useState([]);
-    const id = match.params.id;
+    const id_id = match.params.id;
     //const ct = '18';
     const dispatch = useDispatch();
+
+    const history=useHistory()
+////
+const [id, setId] = useState(null)
+const [name, setName] = useState(null)
+const [content_type, setContent_type] = useState(null)
+const [object_id, setObject_id] = useState(null)
+const [author, setAuthor] = useState(null)
+//const [created_at, setCreated_at] = useState(null)
+///
 
     useEffect(()=> {
         dispatch(fetchParticularUser())
@@ -26,12 +36,44 @@ function CommentDetail({ match }) {
     useEffect(()=>{
         axios({
             method:'GET',
-            url:`http://127.0.0.1:8000/api/comments/${id}`,
+            url:`http://127.0.0.1:8000/api/comments/${id_id}`,
         }).then(response=>{
             setComment(response.data)
+            setId(response.data.id);
+            setName(response.data.name);
+            setAuthor(response.data.author);
+            setContent_type(response.data.content_type);
+            setObject_id(response.data.object_id);
+            //setAddress(result.data.address);
         })
     },[id])
 
+
+
+    /////////////
+
+   const UpdateCom = async () => {
+        let formField = new FormData()
+
+        formField.append('name',name)
+        formField.append('id',id)
+        formField.append('content_type',content_type)
+        formField.append('object_id',object_id)
+        formField.append('author',author)
+        //formField.append('created_at',created_at)
+        
+        await axios({
+            method: 'PUT',
+            url: `http://127.0.0.1:8000/api/comments/${id_id}/`,
+            data: formField
+        }).then(response => {
+            console.log(response.data);
+            history.push("/");
+        })
+
+    }
+
+//////////////////////////////
 
     return(
         <div>
@@ -40,7 +82,18 @@ function CommentDetail({ match }) {
             <h3>created at {comment.created_at}</h3>
             <Link to={{ pathname: `/update/${id}/`, fromDashboard: false}}>update</Link>
             <hr/>
-            <DeleteComment id={id}/>    
+            <DeleteComment id={id}/>   
+            <div className="form-group">
+        <input
+          type="text"
+          className="form-control form-control-lg"
+          placeholder="Enter Your Name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+            <button onClick={UpdateCom} >Update</button> 
         </div>
     )
 }
